5d9a5385204728848d05958465979557cb209dfd
dulcieq <dquinn6@mail.pima.edu>
2024-06-11 18:44:13 -0600

    Update cis129_lab02_shortStory.py
    Adds the story and description for the lab.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,10 @@
-print('Hello, World!')
+#Author: Dulcie Quinn
+#My first Python lab in my CIS129 course
+#prints a simple story
+
+print('There once was a kid who thought she had her deadlines in her head.')
+print('But one day she opened her computer to work.')
+print('And sadly, she was wrong.')
+print('Lesson learned, double check and don't assume.')
+print('She won't make the same mistake twice.')
+
