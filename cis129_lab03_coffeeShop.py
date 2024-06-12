8b89b769425aecc97d0950a6b8f98e89c61c2a2c
dulcieq <dquinn6@mail.pima.edu>
2024-06-12 16:43:14 -0600

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -15,7 +15,9 @@ print('             ')
 print('╔══ ❀•°❀°•❀ ══╗ ')
 print('     MENU  ')
 print('  Coffee - $5')
+print('  Tea    - $5')
 print('  Muffin - $4')
+print('  Pupcup - $1')
 print('╚══ ❀•°❀°•❀ ══╝ ')
 print('         ')
 print('         ')
@@ -26,17 +28,29 @@ coffee = input("How many coffees would you like? ")
 
 coffee = int(coffee)    #user inputs # of coffees wanted
 
+tea = input("How many teas would you like?")
+
+tea = int(tea) #user inputs # of teas wanted
+
 muffin = input("How many muffins would you like? ")
 
 muffin = int(muffin)    #user inputs # of muffins wanted
+
+pupcup = input("How many teas would you like? ") #user inputs # of pupcups wanted
+
+pupcup = int(pupcup) 
 
 #calculate prices
 
 cof_price = coffee * 5       #amount of coffees times price
 
+tea_price = tea * 5 # amt tea times price
+
 muf_price = muffin * 4       #amount of muffins times price
 
-subtotal = cof_price + muf_price    #total price for coffees and muffins w/o tax
+pup_price = pupcup * 1 #amt pupcup times price
+
+subtotal = cof_price + muf_price + tea_price + pup_price   #total price for coffees and muffins w/o tax
 
 tax_price = subtotal * 0.06         #6% tax on subtotal
 
@@ -59,7 +73,9 @@ print('✯¸.•´*¨`*•✿ ✿•*`¨*`•.¸✯')
 print('       ')
 print('My Coffee and Muffin Shop Receipt')
 print(coffee,'Coffee at $5 each: $', cof_price)
-print(muffin, 'Muffin at $4 each: $', muf_price)           
+print(tea,'Tea at $5 each: $', tea_price)
+print(muffin, 'Muffin at $4 each: $', muf_price) 
+print(pupcup,'Pupcups at $1 each: $', pup_price)
 print('Subtotal: $', subtotal)
 print('6% tax: $', tax_price)
 print('       ')
